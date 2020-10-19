0a28682bd725fedafc37101615e42689bc6224e3
Adam Petrashkevich <adasechkaa@gmail.com>
2020-10-19 22:47:01 +0300

    feat: end this solution

--- src/index.js
+++ src/index.js
@@ -1,91 +1,103 @@
 module.exports = function toReadable (number) {
-  let finishArr = '';
-  let startArr = number.toString().split('');
-  startArr.forEach(function(item, i, startArr) {
-    if (startArr[i] == 0) {
-      switch (item) {
-        case 1:
-          return 'one hundred';
-          break;
-        case 2:
-          return 'two hundred';
-          break;
-        case 3:
-          return 'three hundred';
-          break;
-        case 4:
-          return 'four hundred';
-          break;
-        case 5:
-          return 'five hundred';
-          break;
-        case 6:
-          return 'six hundred';
-          break;
-        case 7:
-          return 'seven hundred';
-          break;
-        case 8:
-          return 'eight hundred';
-          break;
-        case 9:
-          return 'nine hundred';
-          break;
-      }
-    } if (startArr[i] == 1) {
-      if (item == 0) {
-        return '';
-      } else if (item == 1) {
-        return deciFunc();
+  return number.toString().split('').reverse().map((item, index, array) => {
+    return index < 2 ? twoFirstNums(item, index, array) : notTwoFirstNums(simpleNumber(item), index, array); 
+  }).reverse().join(' ').reduceWhiteSpace().trim();
+  
+  function twoFirstNums(item, index, array) {
+    if (index == 0) {
+      if (!array[1] && item == '0') return 'zero';
+      if (array[1] == '0' || !array[1]) {
+        return simpleNumber(item);
+      } else if (array[1] == '1') {
+        return decimNumber(item);
       } else {
-        switch (item) {
-          case 2:
-            return 'twenty'
-            break;
-          case 3:
-            return 'thirty'
-            break;
-          case 4:
-            return 'forty'
-            break;
-          case 5:
-            return 'fifty'
-            break;
-          case 6:
-            return 'sixty'
-            break;
-          case 7:
-            return 'seventy'
-            break;
-          case 8:
-            return 'eighty'
-            break;
-          case 9:
-            return 'ninety'
-            break;
-        }
+        return decimNumberAfterOne(array[1], simpleNumber(item));
       }
-    }
-  });
-  function deciFunc() {
-    if (startArr[2] == 1) {
-      return 'eleven';
-    } else if (startArr[2] == 2) {
-      return 'twelve';
-    } else if (startArr[2] == 3) {
-      return 'thirteen';
-    } else if (startArr[2] == 4) {
-      return 'fourteen';
-    } else if (startArr[2] == 5) {
-      return 'fifteen';
-    } else if (startArr[2] == 6) {
-      return 'sixteen';
-    } else if (startArr[2] == 7) {
-      return 'seventeen';
-    } else if (startArr[2] == 8) {
-      return 'eighteen';
-    } else if (startArr[2] == 9) {
-      return 'nineteen';
     }
   }
+
+  function notTwoFirstNums(item, index, array) {
+    switch (index) {
+      case 2:
+        return item + ' hundred'
+    }
+  }
+
+  function simpleNumber(item) {
+    switch (item) {
+      case '0':
+        return '';
+      case '1':
+        return 'one';
+      case '2':
+        return 'two';
+      case '3':
+        return 'three';
+      case '4':
+        return 'four';
+      case '5':
+        return 'five';
+      case '6':
+        return 'six';
+      case '7':
+        return 'seven';
+      case '8':
+        return 'eight';
+      case '9':
+        return 'nine';
+      default:
+        break;
+    }
+  }
+
+  function decimNumber(item) {
+      switch (item) {
+        case '0':
+          return 'ten';
+        case '1':
+          return 'eleven';
+        case '2':
+          return 'twelve';
+        case '3':
+          return 'thirteen';
+        case '4':
+          return 'fourteen';
+        case '5':
+          return 'fifteen';
+        case '6':
+          return 'sixteen';
+        case '7':
+          return 'seventeen';
+        case '8':
+          return 'eighteen';
+        case '9':
+          return 'nineteen';
+        default:
+          break;
+      }
+  }
+  function decimNumberAfterOne(fNum, tNum) {
+      switch (fNum) {
+        case '2':
+          return 'twenty ' + tNum;
+        case '3':
+          return 'thirty ' + tNum;
+        case '4':
+          return 'forty ' + tNum;
+        case '5':
+          return 'fifty ' + tNum;
+        case '6':
+          return 'sixty ' + tNum;
+        case '7':
+          return 'seventy ' + tNum;
+        case '8':
+          return 'eighty ' + tNum;
+        case '9':
+          return 'ninety ' + tNum;
+      }
+  }
 }
+
+String.prototype.reduceWhiteSpace = function() {
+  return this.replace(/\s+/g, ' ');
+};
